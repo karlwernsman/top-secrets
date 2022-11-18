819c1f7f7eb993422e76d074136aa0f98cf69149
karl wernsman <wernsmank@gmail.com>
2022-11-18 09:46:25 -0800

    /POST creates user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,31 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const fakeUser = {
+  firstName: 'Lottie',
+  lastName: 'Dog',
+  email: 'lottie@lottie.com',
+  password: 'lottie',
+};
+
+describe('top-secret routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/POST creates user', async () => {
+    const res = await request(app).post('/api/v1/users').send(fakeUser);
+    const { firstName, lastName, email } = fakeUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
