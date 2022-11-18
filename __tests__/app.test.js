77a8ed056aeef015551e625f134f904c57cf4bbd
karl wernsman <wernsmank@gmail.com>
2022-11-18 10:35:13 -0800

    /POST signs in user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,23 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
 
 const fakeUser = {
   firstName: 'Lottie',
   lastName: 'Dog',
   email: 'lottie@lottie.com',
   password: 'lottie',
+};
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? fakeUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...fakeUser, ...userProps });
+
+  const { email } = user;
+
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('top-secret routes', () => {
@@ -23,6 +34,16 @@ describe('top-secret routes', () => {
       firstName,
       lastName,
       email,
+    });
+  });
+  it('/POST logs in the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
 
